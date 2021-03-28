0bd6635dc9bc69efbceb98ab4b5204dfa79f407d
akilds <akilrainadesigan@gmail.com>
2021-03-28 20:37:39 +0530

    Multiple Person

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -10,26 +10,49 @@ class ContactPerson
      public String state;
      public String zip;
      public String email;
-     public ContactPerson()
+     public ContactPerson(String firstname, String lastname, String phoneNo,String address, String city, String state, String zip, String email)
      {
-         Scanner scan = new Scanner(System.in);
-         System.out.println("Enter firstname : ");
-         this.firstname = scan.nextLine();
-         System.out.println("Enter lastname : ");
-         this.lastname = scan.nextLine();
-         System.out.println("Enter phoneNo : ");
-         this.phoneNo = scan.nextLine();
-         System.out.println("Enter address : ");
-         this.address = scan.nextLine();
-         System.out.println("Enter city : ");
-         this.city = scan.nextLine();
-         System.out.println("Enter state : ");
-         this.state = scan.nextLine();
-         System.out.println("Enter zip : ");
-         this.zip = scan.nextLine();
-         System.out.println("Enter email : ");
-         this.email = scan.nextLine();
+         this.firstname = firstname;
+         this.lastname = lastname;
+         this.phoneNo = phoneNo;
+         this.address = address;
+         this.city = city;
+         this.state = state;
+         this.zip = zip;
+         this.email = email;
       }
+     void changefname(String fname)
+     {
+      this.firstname = fname;
+     }
+     void changelname(String lname)
+     {
+      this.lastname = lname;
+     }
+     void changephoneno(String phoneno)
+     {
+      this.phoneNo = phoneno;
+     }
+     void changeaddress(String addrs)
+     {
+      this.address = addrs;
+     }
+     void changecity(String citty)
+     {
+      this.city = citty;
+     }
+     void changestate(String statee)
+     {
+      this.state = statee;
+     }
+     void changezip(String zipp)
+     {
+      this.zip = zipp;
+     }
+     void changemail(String mail)
+     {
+      this.email = mail;
+     }
      public void show()
      {
 	       System.out.println("Contact Details");
@@ -44,45 +67,71 @@ class ContactPerson
      }
 }
 
-
-
 public class AddressBookMain
 {
 	public static void main(String[] args)
    {
-	   ContactPerson[] contact = new ContactPerson[10];
+	   ArrayList<ContactPerson> contact = new ArrayList<ContactPerson>();
+      String fname;
+      String lname;
+      String phoneno;
+      String addrs;
+      String cityy;
+      String statee;
+      String zipp;
+      String mail;
+      char c = 'Y';
+      int i=0;
+      int count = 0;
 	   Scanner sc = new Scanner(System.in);
-	   System.out.println("Enter number of contacts : ");
-	   int n = sc.nextInt();
-	   for(int i=0;i<n;i++)
-	   {
-		   contact[i] = new ContactPerson();
-	   }
-	   for(int i=0;i<n;i++)
-	   {
-		   contact[i].show();
-      }
-      System.out.println("1. Edit");
-      System.out.println("2. Delete");
-      System.out.println("Enter choice : ");
-      int choice = sc.nextInt();
-      if(choice==1)
+      do
       {
+       System.out.println("1. ADD");
+       System.out.println("2. EDIT");
+       System.out.println("3. DELETE");
+       System.out.println("Enter choice : ");
+       int choice = sc.nextInt();
+       if(choice==1)
+       {
+         Scanner scan = new Scanner(System.in);
+         System.out.println("Enter firstname : ");
+         fname = scan.nextLine();
+         System.out.println("Enter lastname : ");
+         lname = scan.nextLine();
+         System.out.println("Enter phoneNo : ");
+         phoneno = scan.nextLine();
+         System.out.println("Enter address : ");
+         addrs = scan.nextLine();
+         System.out.println("Enter city : ");
+         cityy = scan.nextLine();
+         System.out.println("Enter state : ");
+         statee = scan.nextLine();
+         System.out.println("Enter zip : ");
+         zipp = scan.nextLine();
+         System.out.println("Enter email : ");
+         mail = scan.nextLine();
+         contact.add(new ContactPerson(fname,lname,phoneno,addrs,cityy,statee,zipp,mail));
+         contact.get(i).show();
+         i++;
+         count++;
+       }
+       else if(choice==2)
+       {
         System.out.println("Enter a name to be edited : ");
 	     sc.nextLine();
         String name = sc.nextLine();
-	     for(int i=0;i<n;i++)
-	     {
-		     if(contact[i].firstname.equals(name))
+        for(i=0;i<count;i++)
+        {
+		     if(contact.get(i).firstname.equals(name))
 		     {
 	   	     System.out.println("What do you want to change : ");
               System.out.println("1. Firstname");
               System.out.println("2. Lastname");
-              System.out.println("3. Address");
-              System.out.println("4. City");
-              System.out.println("5. State");
-              System.out.println("6. Zip");
-              System.out.println("7. PhoneNo");
+              System.out.println("3. PhoneNo");
+              System.out.println("4. Address");
+              System.out.println("5. City");
+              System.out.println("6. State");
+              System.out.println("7. Zip");
               System.out.println("8. Email");
               System.out.println("Enter your choice : ");
 		   	  int option = sc.nextInt();
@@ -90,54 +139,60 @@ public class AddressBookMain
 			     switch(option)
 			     {
 			       case 1 : System.out.println("Enter new firstname : ");
-                         contact[i].firstname = sc.nextLine();
+                         fname = sc.nextLine();
+                         contact.get(i).changefname(fname);
 			                break;
 		     	    case 2 : System.out.println("Enter new lastname : ");
-                         contact[i].lastname = sc.nextLine();
+                         lname = sc.nextLine();
+                         contact.get(i).changelname(lname);
 			                break;
-			       case 3 : System.out.println("Enter new address : ");
-                         contact[i].phoneNo = sc.nextLine();
+			       case 3 : System.out.println("Enter new phonoNo : ");
+                         phoneno = sc.nextLine();
+                         contact.get(i).changephoneno(phoneno);
                          break;
-				    case 4 : System.out.println("Enter new city : ");
-                         contact[i].address = sc.nextLine();
+				    case 4 : System.out.println("Enter new address : ");
+                         addrs = sc.nextLine();
+                         contact.get(i).changeaddress(addrs);
                          break;
-			       case 5 : System.out.println("Enter new state : ");
-                         contact[i].city = sc.nextLine();
+			       case 5 : System.out.println("Enter new city : ");
+                         cityy = sc.nextLine();
+                         contact.get(i).changecity(cityy);
 			                break;
-			       case 6 : System.out.println("Enter new zip : ");
-                         contact[i].state = sc.nextLine();
+			       case 6 : System.out.println("Enter new state : ");
+                         statee = sc.nextLine();
+                         contact.get(i).changestate(statee);
                          break;
-		     	    case 7 : System.out.println("Enter new phoneno : ");
-                         contact[i].zip = sc.nextLine();
+		     	    case 7 : System.out.println("Enter new zip : ");
+                         zipp = sc.nextLine();
+                         contact.get(i).changezip(zipp);
 			                break;
 			       case 8 : System.out.println("Enter new email : ");
-                         contact[i].email = sc.nextLine();
+                         mail = sc.nextLine();
+                         contact.get(i).changemail(mail);
                          break;
 			       default: System.out.println("Invalid");
 			     }
-			    contact[i].show();
+              contact.get(i).show();
 		     }
- 	      }
+          }
         }
         else
         {
           System.out.println("Enter the name to be deleted : ");
           sc.nextLine();
           String del_name = sc.nextLine();
-          for(int i=0; i<n; i++)
+          for(i=0;i<count;i++)
           {
-            if(contact[i].firstname.equals(del_name))
+            if(contact.get(i).firstname.equals(del_name))
             {
-              contact[i] = null ;
-             }
+              contact.remove(i);
+              count--;
+            }
+           contact.get(0).show();
           }
-          for(int i=0;i<n;i++)
-          {
-             if(contact[i]!=null)
-             {
-             contact[i].show();
-             }
-          }
-        }
+         }
+         System.out.println("Do you want to continue (Y/N) : ");
+         c = sc.next().charAt(0);
+     }while(c=='Y'||c=='y');
    }
 }
